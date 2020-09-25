6b0720e4f5b4564c32a045a7413e758624511a77
Arun K <arundas.k.92@gmail.com>
2020-09-25 10:57:45 -0700

    Implement seeding of images from local machine

--- database/seeder.js
+++ database/seeder.js
@@ -1,8 +1,7 @@
 const faker = require('faker');
 const { saveAgentData } = require('.');
 
-
-const createData = function(num) {
+const createData = (num) => {
   const agentTypes = ['Listing Agent', 'Premier Agent'];
   const propertyIdArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   for (let i = 100; i < num + 100; i++) {
@@ -13,9 +12,9 @@ const createData = function(num) {
     data.phone = faker.phone.phoneNumber();
     data.email = faker.internet.email();
     data.agentType = agentTypes[Math.floor(Math.random() * agentTypes.length)];
-    data.propertyIds = propertyIdArray.splice(Math.floor(Math.random() * propertyIdArray.length));
+    data.propertyIds = propertyIdArray.slice(Math.floor(Math.random() * propertyIdArray.length));
     data.recSalesCount = Math.floor(Math.random() * 100);
-    data.avgRating = Math.floor(Math.random() * 5.0);
+    data.avgRating = (Math.random() * 5.0).toFixed(1);
     data.ratingCount = Math.floor(Math.random() * 50);
     saveAgentData(data);
   }
